4b8c7e55f36cca877f03a4fe86b84d0efdbf0db7
Leetzner <ognill@hotmail.com>
2023-01-25 16:10:32 +0000

    added products views and templates

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Category(models.Model):
 
     class Meta:
-        verbose_name_plural= 'Categories'
-
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +14,7 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
