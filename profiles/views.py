2dc78856bd5e0021c3fad379789d8aba1820bf18
Leetzner <ognill@hotmail.com>
2023-02-09 12:01:47 +0000

    Product admin - finished add product functionallity

--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
@@ -28,6 +30,7 @@ def profile(request):
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
